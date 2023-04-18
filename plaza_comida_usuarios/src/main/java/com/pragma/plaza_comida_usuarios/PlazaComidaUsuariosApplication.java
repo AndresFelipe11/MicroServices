333c8711ce3b11725ad13440d3fb2b43e36de3c8
Andres Felipe Cadavid <andres.1701714898@gmail.com>
2023-04-17 20:02:20 -0500

    Usuarios

---
+++ plaza_comida_usuarios/src/main/java/com/pragma/plaza_comida_usuarios/PlazaComidaUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.plaza_comida_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlazaComidaUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlazaComidaUsuariosApplication.class, args);
+	}
+
+}
---
+++ plaza_comida_usuarios/src/test/java/com/pragma/plaza_comida_usuarios/PlazaComidaUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.plaza_comida_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlazaComidaUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
