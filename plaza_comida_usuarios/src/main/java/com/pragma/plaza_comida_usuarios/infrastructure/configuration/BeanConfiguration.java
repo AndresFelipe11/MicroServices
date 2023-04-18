c22aebdb3cdcc3956a650c7af460fba061f3c675
Andres Felipe Cadavid <andres.1701714898@gmail.com>
2023-04-18 10:45:03 -0500

    comit inicial para crear usuarios

--- plaza_comida_usuarios/src/main/java/com/pragma/plaza_comida_usuarios/infrastructure/configuration/BeanConfiguration.java
+++ plaza_comida_usuarios/src/main/java/com/pragma/plaza_comida_usuarios/infrastructure/configuration/BeanConfiguration.java
@@ -1,33 +1,37 @@
-// package com.pragma.plaza_comida_usuarios.infrastructure.configuration;
+package com.pragma.plaza_comida_usuarios.infrastructure.configuration;
 
-// import lombok.RequiredArgsConstructor;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.security.crypto.password.PasswordEncoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-// import com.pragma.plaza_comida_usuarios.domain.api.IUserServicePort;
-// import com.pragma.plaza_comida_usuarios.domain.spi.IUserPersistencePort;
+import com.pragma.plaza_comida_usuarios.domain.api.IUserServicePort;
+import com.pragma.plaza_comida_usuarios.domain.spi.IUserPersistencePort;
+import com.pragma.plaza_comida_usuarios.domain.usecase.UserUseCase;
+import com.pragma.plaza_comida_usuarios.infrastructure.out.jpa.adapter.UserJpaAdapter;
+import com.pragma.plaza_comida_usuarios.infrastructure.out.jpa.mapper.IUserEntityMapper;
+import com.pragma.plaza_comida_usuarios.infrastructure.out.jpa.repository.IUserRepository;
 
-// @Configuration
-// @RequiredArgsConstructor
-// public class BeanConfiguration {
-//     private final IUserRepository userRepository;
-//     private final IUserEntityMapper userEntityMapper;
+@Configuration
+@RequiredArgsConstructor
+public class BeanConfiguration {
+    private final IUserRepository userRepository;
+    private final IUserEntityMapper userEntityMapper;
 
-//     @Bean
-//     public PasswordEncoder passwordEncoder() {
-//         return new BCryptPasswordEncoder();
-//     }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-//     @Bean
-//     public IUserPersistencePort userPersistencePort() {
-//         return new UserJpaAdapter(userRepository, userEntityMapper);
-//     }
+    @Bean
+    public IUserPersistencePort userPersistencePort() {
+        return new UserJpaAdapter(userRepository, userEntityMapper);
+    }
 
-//     @Bean
-//     public IUserServicePort userServicePort() {
-//         return new UserUseCase(userPersistencePort());
-//     }
+    @Bean
+    public IUserServicePort userServicePort() {
+        return new UserUseCase(userPersistencePort());
+    }
 
-// }
+}
