f23b156a60659c3515fa8e7b2aac00481dc034b5
Andres Felipe Cadavid <andres.1701714898@gmail.com>
2023-04-18 19:25:38 -0500

    creacion de usuarios funcionando

--- plaza_comida_usuarios/src/main/java/com/pragma/plaza_comida_usuarios/PlazaComidaUsuariosApplication.java
+++ plaza_comida_usuarios/src/main/java/com/pragma/plaza_comida_usuarios/PlazaComidaUsuariosApplication.java
@@ -3,11 +3,7 @@ package com.pragma.plaza_comida_usuarios;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
-@EnableEurekaClient
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class PlazaComidaUsuariosApplication {
 
