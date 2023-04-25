c13c11fb2a016a301d77904e4ac6b1bc30e872da
Andres Felipe Cadavid <andres.1701714898@gmail.com>
2023-04-25 13:50:52 -0500

    config microservices

--- plaza_comida_usuarios/src/main/java/com/pragma/plaza_comida_usuarios/PlazaComidaUsuariosApplication.java
+++ plaza_comida_usuarios/src/main/java/com/pragma/plaza_comida_usuarios/PlazaComidaUsuariosApplication.java
@@ -3,8 +3,16 @@ package com.pragma.plaza_comida_usuarios;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+
+@EnableEurekaClient
+@EnableFeignClients
+@EnableGlobalMethodSecurity(jsr250Enabled=true)
+@SpringBootApplication
+
 public class PlazaComidaUsuariosApplication {
 
 	public static void main(String[] args) {
